1daae37f9e1f2e964274b9c6939e54917c6eabce
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-02-27 11:38:27 +0530

    Added GMX <> Socket Logo in the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,11 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export const Header = () => {
 	return (
 		<div className="flex justify-between shadow-[0_12px_15px_0_rgb(0,0,0,0.25)] border-b border-solid border-[#181B29]">
-			<div className="pt-5 pb-4 pl-4 text-lg text-white font-semibold">
-				Socket Logo
-			</div>
+			<img
+				alt="GMX <> Socket Logo"
+				src="assets/GMXxSocket.svg"
+				className="ml-2"
+			/>
 			<div className="pt-5 pb-4 pr-4">
 				<ConnectButton showBalance={false} />
 			</div>
